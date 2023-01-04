b19663f8401363021de24194ac2154434c1abe36
Sammwy <sammwy.dev@gmail.com>
2023-01-04 11:01:16 -0300

    feature: initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,29 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import type { AppProps } from "next/app";
 
+// Theme
+const colors = {
+  transparent: "transparent",
+};
+
+const styles = {
+  global: {
+    "html, body": {
+      background: "#0B0916", // #11101D
+      color: "gray.200",
+    },
+  },
+};
+
+const theme = extendTheme({
+  colors,
+  styles,
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
