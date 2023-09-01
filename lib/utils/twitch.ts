dc331eacb4debcf9488db162da59ce9e8d11355e
Sammwy <sammwy.dev@gmail.com>
2023-09-01 00:11:23 -0300

    feature: dynamically generate redirect uri for oauth

--- lib/utils/twitch.ts
+++ lib/utils/twitch.ts
@@ -1,13 +1,23 @@
 import { TwitchOAuth } from "twitch-oauth";
 
+const isBrowser =
+  typeof globalThis !== "undefined" &&
+  typeof globalThis["window"] !== "undefined";
+
+const getRedirectURI = () => {
+  if (isBrowser) {
+    const proto = window.location.protocol;
+    const host = window.location.host;
+    return `${proto}//${host}/oauth/twitch`;
+  } else {
+    return `http://localhost:3000/`;
+  }
+};
+
 const twitch = new TwitchOAuth({
   clientId: process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID as string,
   clientSecret: process.env.TWITCH_CLIENT_SECRET as string,
-  redirectUri: `${
-    process.env.VERCEL
-      ? "https://" + process.env.VERCEL_URL
-      : "http://localhost:3000"
-  }/oauth/twitch`,
+  redirectUri: getRedirectURI(),
   scope: ["user:read:email"],
   method: "code",
 });
